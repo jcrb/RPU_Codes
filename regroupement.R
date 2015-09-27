8dc7ffa6ec99b8c70d55ba215dea8376d313e802
JCB <jeanclaude.bartier@gmail.com>
2015-09-27 18:22:28 +0200

    maj

--- regroupement.R
+++ regroupement.R
@@ -14,7 +14,7 @@
 #' @param d dataframe source. Doit contenir au moins une colonne CHAPITRE. 
 #'Dans l'exemple d3 correspond à tous les enregistrement, d3g pour la gériatrie,
 #'d3p pour la pédiatrie, etc.
-#' @param groupe: une des 5 valeurs autorisées: "medic", "trau", "psy", "toxico", "autre"
+#' @param groupe: une des 5 valeurs autorisées: "medchir", "trau", "psy", "toxico", "autre"
 #' @usage f <- pathologie.medicochir(d3, "autre"); f$n; f$table; f$prop; f$data;
 #'tab1(f$data, cex.names = 0.5, cex = 0.8, 
 #   sort.group = "decreasing", main = "Médico-chir adultes", bar.values = "percent")
@@ -122,4 +122,54 @@ summary.toxicologie <- function(dx){
     n <- sum(t)
     
     return(t)
+}
+
+#==========================================================================
+#
+#   summary.medicochir
+#
+#==========================================================================
+#' @title résumé de la partie médico-chirurgicale
+#' @description
+#' @param dx un dataframe de typr d3
+#' 
+summary.medicochir <- function(dx){
+  vx <- dx[dx$TYPE.URGENCES == "Médico-chirurgical", "CHAPITRE"]
+  n <- length(vx) # nb de valeurs
+  n.na <- sum(is.na(vx)) # nb de valeurs non renseignées
+  p.na <- mean(is.na(vx)) # % de valeurs non renseignées
+  n.rens <- sum(!is.na(vx)) # nb de valeurs renseignées
+  p.rens <- mean(!is.na(vx)) # % de valeurs renseignées
+  
+  s <- table(vx)
+  n.cardio <- s['Douleurs thoraciques, pathologies cardio-vasculaires']
+  n.neuro <- s['Céphalées, pathologies neurologiques hors SNP']
+  n.respi <- s['Dyspnées, pathologies des voies aériennes inférieures']
+  n.dig <- s['Douleurs abdominales, pathologies digestives']
+  n.inf <- s['Fièvre et infectiologie générale']
+  n.malaise <- s['Malaises, lipothymies, syncopes, étourdissements et vertiges']
+  n.dermato <- s['Dermato-allergologie et atteintes cutanéo-muqueuses']
+  n.uro <- s['Douleurs pelviennes, pathologies uro-génitales']
+  n.orl <- s['ORL, ophtalmo, stomato et carrefour aéro-digestif']
+  
+  p.cardio <- n.cardio / n.rens
+  p.neuro <- n.neuro / n.rens
+  p.respi <- n.respi / n.rens
+  p.dig <- n.dig / n.rens
+  p.inf <- n.inf / n.rens
+  p.malaise <- n.malaise / n.rens
+  p.dermato <- n.dermato / n.rens
+  p.uro <- n.uro / n.rens
+  p.orl <- n.orl / n.rens
+  
+  
+  
+  a <- c(n, n.na, p.na, n.rens, p.rens, n.cardio, n.neuro, n.respi, n.dig, n.inf, n.malaise, n.dermato, n.uro, n.orl,
+         p.cardio, p.neuro, p.respi, p.dig, p.inf, p.malaise, p.dermato, p.uro, p.orl)
+  
+  names(a) <- c("n", "n.na", "p.na", "n.rens", "p.rens", "n.cardio", "n.neuro", "n.respi", "n.dig", "n.inf", "n.malaise", 
+                "n.dermato", "n.uro", "n.orl", "p.cardio", "p.neuro", "p.respi", "p.diges", "p.infec", "p.malaise", "p.dermato", 
+                "p.uro", "p.orl")
+  
+  return(a)
 }
